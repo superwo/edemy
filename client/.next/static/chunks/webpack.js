d00f1cdf1432d51fbb77e7eae48cf5b35cea02b2
Radu <avadanii@tiscali.it>
2021-08-28 18:56:42 +0300

    working

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1272e0e4febec13acc42"; }
+/******/ 		__webpack_require__.h = function() { return "96763ece8b2628e2fe95"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/pages/register.js
+++ client/pages/register.js
@@ -1,13 +1,20 @@
+import axios from "axios";
 import { useState } from "react";
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setName] = useState("Radu");
+  const [email, setEmail] = useState("avadanii@tiscali.it");
+  const [password, setPassword] = useState("artemida");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.table({ name, email, password });
+
+    const { data } = await axios.post(`http://localhost:8000/api/register`, {
+      name,
+      email,
+      password,
+    });
+    console.log("REGISTER RESPONSE", data);
   };
 
   return (
